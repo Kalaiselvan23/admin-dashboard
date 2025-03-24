1b3f5b3795374ffafd1a6928c74960d18a054c7f
kalaiselvan23 <kalaiselvan1623@gmail.com>
2025-03-24 16:24:35 +0530

    fixed merge issues

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { RouterProvider } from 'react-router-dom'
 import router from './router';
 import { ThemeProvider } from './components/theme-provider'
@@ -14,48 +13,5 @@ export function App() {
         </AuthProvider>
       </QueryClientProvider>
     </ThemeProvider>
-=======
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
-
-export function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
->>>>>>> 51730c68f1135db22f56d6d5c6f8b1c3e0fd59bb
   )
 }
